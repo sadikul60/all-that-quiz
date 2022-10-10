05b172fb8c9652c6283d789dc119fb89381bac3b
mdsadikul <mdsadikulislam891@gamil.com>
2022-10-11 01:59:39 +0600

    Added Navbar active class

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import './Navbar.css';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
