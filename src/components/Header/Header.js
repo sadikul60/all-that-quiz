ccb1a7c4d455850a696971a0b17096404fdbd986
mdsadikul <mdsadikulislam891@gamil.com>
2022-10-11 13:02:58 +0600

    Added header bg and title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import background from '../../top.png'
 
 const Header = () => {
     return (
-        <div className=''>
-            <img className= "w-full h-72 " src="top.png" alt="" />
+        <div>
+            <div className='w-full h-72' style={{ backgroundImage: `url(${background})`, backgroundSize:'cover', backgroundRepeat: 'no-repeat'}}>
+            <h1 className='text-5xl text-black font-bold py-10 hover:text-indigo-800'>Simple Question & Answere Library</h1>
+            </div>
         </div>
     );
 };
