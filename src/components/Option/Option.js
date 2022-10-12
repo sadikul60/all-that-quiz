669916430d35ed5ae5dc547b6e062a026dccde59
mdsadikul <mdsadikulislam891@gamil.com>
2022-10-12 11:09:47 +0600

    added option text and radio items-center

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -19,8 +19,8 @@ const Option = ({ option, clickedButton  }) => {
         
         <div>
             <label  className="label cursor-pointer rounded-md  hover:bg-blue-500 bg-slate-400 my-4 px-4 md:px-8 w-3/4 mx-auto">
-                <button onClick={() => handleClickedOption(option)} className='flex justify-between text-center text-white w-full mx-auto '>
-                    <span className=" text-sm text-left md:text-xl px-2 md:px-5">{option}</span> 
+                <button onClick={() => handleClickedOption(option)} className='flex justify-between items-center text-center text-white w-full mx-auto '>
+                    <span className=" text-sm text-left md:text-xl px-2 md:px-4">{option}</span> 
                     <input type="radio" name='radio-6' className=" text-slate-50 w-6 h-6 checked:bg-blue-500 " checked />
                 </button>
             </label>
